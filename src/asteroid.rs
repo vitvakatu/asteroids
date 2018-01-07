cdebadb26eb081e0c37deea42ac850c287b99dfb
Ilya Bogdanov <fumlead@gmail.com>
2018-01-07 18:04:24 +0300

    Spawning asteroids

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -7,10 +7,14 @@ use rand::Rng;
 use euler::{Quat, Vec2};
 use std::f32::consts::PI;
 
+use world_to_screen;
+
 #[derive(Clone)]
 pub struct Asteroid {
 	pos: Vec2,
 	vel: Vec2,
+	size: u8,
+	hitpoints: u8,
 	group: three::Group,
 }
 
@@ -31,24 +35,55 @@ impl Object for Asteroid {}
 impl Asteroid {
 	pub fn new(factory: &mut three::Factory, pos: Vec2, size: u8) -> Self {
 		let mut rng = rand::thread_rng();
-		let radius = size as f32 * 0.2;
+		let radius = size as f32 * 0.1;
 		let geometry = three::Geometry::cylinder(radius, radius, 0.01, 32);
 		let material = three::material::Wireframe { color: rng.next_u32() };
 		let mesh = factory.mesh(geometry, material);
 		let group = factory.group();
 		mesh.set_orientation(Quat::euler(vec3!(PI / 2.0, 0.0, 0.0)));
 		group.add(&mesh);
-		let vel_x = rng.gen_range(0.05, 0.5);
-		let vel_y = rng.gen_range(0.05, 0.5);
+		let vel_x = rng.gen_range(0.01, 0.2);
+		let vel_y = rng.gen_range(0.01, 0.2);
 		Self {
 			pos,
 			vel: vec2!(vel_x, vel_y),
+			size,
+			hitpoints: size + rng.gen_range(0, 2),
 			group,
 		}
 	}
 
-	pub fn update(&mut self, input: &three::Input) {
-		self.pos += self.vel * input.delta_time();
+	pub fn update(&mut self, window: &three::Window) {
+		self.pos += self.vel * window.input.delta_time();
+
+		// Check window borders
+		let window_size = window.size();
+		let mut screen_pos = world_to_screen(self.pos, window_size.into());
+		if screen_pos.x < 0.0 {
+			screen_pos.x += window_size.x;
+		} else if screen_pos.x > window_size.x {
+			screen_pos.x -= window_size.x;
+		}
+		if screen_pos.y < 0.0 {
+			screen_pos.y += window_size.y;
+		} else if screen_pos.y > window_size.y {
+			screen_pos.y -= window_size.y;
+		}
+		self.pos = window.renderer.map_to_ndc(screen_pos).into();
+
 		self.set_position(vec3!(self.pos, 0.0));
+	}
+
+	pub fn apply_damage(&mut self, factory: &mut three::Factory) -> (u32, Vec<Asteroid>) {
+		self.hitpoints -= 1;
+		let mut score = 0;
+		if self.hitpoints <= 0 {
+			score += self.size as u32 * 100;
+			let number_of_debris = rand::thread_rng().gen_range(0, 2);
+			let vec = (0..number_of_debris).map(|_| Asteroid::new(factory, self.pos, self.size - 1)).collect::<Vec<_>>();
+			(score, vec)
+		} else {
+			(0, vec![])
+		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate three;
 extern crate euler;
 extern crate rand;
 
+use rand::Rng;
+
 use euler::Vec2;
 use three::Object;
 
@@ -17,6 +19,17 @@ pub fn world_to_screen(coord: Vec2, screen: Vec2) -> Vec2 {
 	vec2!((1.0 + coord.x) * screen.x / 2.0, (1.0 - coord.y) * screen.y / 2.0)
 }
 
+fn find_place_to_spawn(aspect: f32) -> Vec2 {
+	let mut rng = rand::thread_rng();
+	let mut x: f32 = 0.0;
+	let mut y: f32 = 0.0;
+	while x.abs() < 0.5 || y.abs() < 0.5 {
+		x = rng.gen_range(-1.0 / aspect, 1.0 / aspect);
+		y = rng.gen_range(-1.0, 1.0);
+	}
+	vec2!(x, y)
+}
+
 fn main() {
     let mut window = three::Window::new("Asteroids");
     let camera = window.factory.orthographic_camera([0.0, 0.0], 1.0, 1.0 .. 100.0);
@@ -25,12 +38,17 @@ fn main() {
     let mut ship = Ship::new(&mut window, None);
     window.scene.add(&ship);
 
-    let mut asteroid = asteroid::Asteroid::new(&mut window.factory, vec2!(0.5, 0.5), 1);
-    window.scene.add(&asteroid);
+    let mut asteroids = vec![];
+
+    for _ in 0..10 {
+	    let mut asteroid = asteroid::Asteroid::new(&mut window.factory, find_place_to_spawn(window.renderer.aspect_ratio()), 1);
+	    window.scene.add(&asteroid);
+	    asteroids.push(asteroid);
+    }
 
     while window.update() && !window.input.hit(three::KEY_ESCAPE) {
     	ship.update(&mut window);
-    	asteroid.update(&window.input);
+    	asteroids.iter_mut().for_each(|a| a.update(&window));
     	window.render(&camera);
     }
 }
